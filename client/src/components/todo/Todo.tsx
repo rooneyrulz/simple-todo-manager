cba9592776168e868b8bcdd814a8a033c7c5ffa4
rooneyrulz <roony0782045336@gmail.com>
2022-05-18 00:05:29 +0530

    code updated

--- client/src/components/todo/Todo.tsx
+++ client/src/components/todo/Todo.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@mui/material/List";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 import { useSelector } from "react-redux";
 import { RootState } from "redux/reducers";
@@ -18,16 +22,70 @@ const useStyles = makeStyles((theme) => ({
 
 const Todo: React.FC = (): JSX.Element => {
   const classes = useStyles();
+  const [key, setKey] = React.useState("");
+  const [data, setData] = React.useState<ITodo[]>([]);
   const todo = useSelector((state: RootState) => state.todo);
+
+  const sortByDateASC = (arr: ITodo[], key: string): ITodo[] => {
+    return arr?.sort((a: any, b: any) => {
+      var dateA = new Date(a[key]).getTime();
+      var dateB = new Date(b[key]).getTime();
+      return dateA > dateB ? 1 : -1;
+    });
+  };
+
+  const sortByDateDSC = (arr: ITodo[], key: string): ITodo[] => {
+    return arr?.sort((a: any, b: any) => {
+      var dateA = new Date(a[key]).getTime();
+      var dateB = new Date(b[key]).getTime();
+      return dateA < dateB ? 1 : -1;
+    });
+  };
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setKey(event.target.value);
+    if (event.target.value === "EXP_ASC") {
+      const sorted = sortByDateASC(data, "expires");
+      setData(sorted);
+    } else if (event.target.value === "EXP_DSC") {
+      const sorted = sortByDateDSC(data, "expires");
+      setData(sorted);
+    } else if (event.target.value === "INST_DSC") {
+      const sorted = sortByDateDSC(data, "created_at");
+      setData(sorted);
+    } else {
+      const sorted = sortByDateASC(data, "created_at");
+      setData(sorted);
+    }
+  };
+
+  React.useEffect(() => {
+    setData(todo.todos);
+  }, [todo.todos]);
 
   return (
     <List className={classes.root}>
+      <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id='simple-select-label'>Sort By</InputLabel>
+        <Select
+          labelId='simple-select-label'
+          id='simple-select'
+          value={key}
+          onChange={handleChange}
+          label='key'
+        >
+          <MenuItem value='INST_ASC'>Insertion (asc)</MenuItem>
+          <MenuItem value='INST_DSC'>Insertion (dsc)</MenuItem>
+          <MenuItem value='EXP_ASC'>Expiration (asc)</MenuItem>
+          <MenuItem value='EXP_DSC'>Expiration (dsc)</MenuItem>
+        </Select>
+      </FormControl>
       {todo.loading ? (
         <AppLoader />
-      ) : todo.todos.length === 0 ? (
+      ) : data?.length === 0 ? (
         <p>No todos found!</p>
       ) : (
-        todo.todos?.map((todo: ITodo) => {
+        data?.map((todo: ITodo) => {
           return <TodoItem todo={todo} key={todo._id} />;
         })
       )}
